00f566f14093345480acb4e6989657d9fc3ac39f
888connor888 <44200751+888connor888@users.noreply.github.com>
2020-02-03 23:14:18 +0530

    del

--- DataScienceMaster_Python/Face Recognition/image.py
+++
@@ -1,8 +0,0 @@
-# simple program to read an image
-import cv2
-img=cv2.imread('dog.png')
-gray=cv2.imread('dog.png',cv2.IMREAD_GRAYSCALE)
-cv2.imshow('Dog Image',img)
-cv2.imshow('Gray Dog Image',gray)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
