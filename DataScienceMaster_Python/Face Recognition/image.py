f2277323bd01a47e2d87b96a3ba70393550b238e
shubham Halder <44200751+888connor888@users.noreply.github.com>
2020-02-04 23:31:04 +0530

    a

--- DataScienceMaster_Python/Face Recognition/image.py
+++ DataScienceMaster_Python/Face Recognition/image.py
@@ -1,8 +1,13 @@
-# simple program to read an image
+ # to read and show an image
 import cv2
-img=cv2.imread('dog.png')
-gray=cv2.imread('dog.png',cv2.IMREAD_GRAYSCALE)
+
+img = cv2.imread('dog.png')
+# read a gray scale image
+gray = cv2.imread('dog.png',cv2.IMREAD_GRAYSCALE)
+
 cv2.imshow('Dog Image',img)
-cv2.imshow('Gray Dog Image',gray)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.imshow('Gray Image of Dog',gray)
+
+cv2.waitKey(0) # zero means wait infinitely (here time is in ms)
+
+cv2.destroyAllWindows() # after waiting for some time the image window is closed
